02c2332eefbb3f41ad758b5691015cd19e5058be
Elise Phan <aliceconnic@gmail.com>
2019-04-28 16:10:17 +0000

    Automatically backed up by Learn

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Todo = props => {
   return (
     <div>
-      <span>{props.text}</span><button>DELETE</button>
+      <div>
+  <span>{props.text}</span><button onClick={() => props.delete(props.text)}>DELETE</button>
+</div>
     </div>
   )
 }
